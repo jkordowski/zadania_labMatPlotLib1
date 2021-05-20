17223e34bb0548570b220245f9cf0bbbdf97a811
JK <jakub.kordowski@student.uwm.edu.pl>
2021-05-20 13:14:56 +0200

    zad5

--- zad5.py
+++ zad5.py
@@ -1,3 +1,14 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import pandas as pd
 
+df = pd.read_csv('iris.data', sep=',', decimal='.', header=None)
+print(df)
+
+colors = np.random.randint(0, 50, len(df.index))
+scale = [np.abs(df[0].iloc[x] - df[1].iloc[x]) * 5 for x in range(len(df.index))]
+
+plt.scatter(df[0], df[1], c=colors, s=scale)
+plt.xlabel('sepal length')
+plt.ylabel('sepal width')
+plt.show()
